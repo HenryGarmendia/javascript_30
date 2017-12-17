2a8b15527da42e472d4e9a8dac1648315b193419
HenryGarmendia <me@henrygarmendia.com>
2017-12-17 03:08:06 -0600

    inplemented  vanilla javascript to play audio files to some keyboard keys

---
+++ 01- JS Drum Kit/app.js
@@ -0,0 +1,24 @@
+window.addEventListener('keydown', function(e){
+    // use a css attribute selector, switch single quote to Backtick to use ES6 template string
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key   = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
+    // check if no audio
+    if (!audio) {return false;} // stop the function from running all together;
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+    key.classList.add('playing');
+});
+
+function remove_transition(e){ 
+    // console.log(e);
+    // skip it if it's not a transform
+    if (e.propertyName !== 'transform') {
+        return false;
+    }
+    this.classList.remove('playing');
+} // remove_transition END
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', remove_transition));
