afac942d893accd142a29d61b43fd7de9b97e601
HenryGarmendia <me@henrygarmendia.com>
2017-12-18 00:38:47 -0600

    finishing up and cleaning files

--- 02- JS Clock/app.js
+++ 02- JS Clock/app.js
@@ -1,6 +1,6 @@
-const second_hand = document.querySelector('.second-hand');
-const min_hand = document.querySelector('.min-hand');
-const hour_hand = document.querySelector('.hour-hand');
+const SECOND_HAND   = document.querySelector('.second-hand');
+const MIN_HAND      = document.querySelector('.min-hand');
+const HOUR_HAND     = document.querySelector('.hour-hand');
 
 function set_date() {
     // get the date obj.
@@ -8,15 +8,16 @@ function set_date() {
 
     const seconds = date.getSeconds();
     const seconds_degrees = ((seconds / 60) * 360) + 90;
-    const mins = date.getMinutes();
-    const mins_degrees = ((mins / 60) * 360) + 90;
-    const hour = date.getHours();
-    const hours_degrees = ((hour / 60) * 360) + 90;
 
-    // second_hand.style.transform = 'rotate(' + seconds_degrees + 'deg' + ')';
-    second_hand.style.transform = `rotate(${seconds_degrees}deg)`;
-    min_hand.style.transform = `rotate(${mins_degrees}deg)`;
-    console.log(mins);
-}
+    const mins = date.getMinutes();
+    const mins_degrees = (((360 / 60) * mins) + (seconds*(360/60)/60)) + 90;
+
+    const hours = date.getHours();
+    const hours_degrees = (((360 / 12) * hours) + (mins*(360/60)/12)) + 90;
+    
+    SECOND_HAND.style.transform = `rotate(${seconds_degrees}deg)`;
+    MIN_HAND.style.transform = `rotate(${mins_degrees}deg)`;
+    HOUR_HAND.style.transform = `rotate(${hours_degrees}deg)`;
+} // set_date END
 
 setInterval(set_date, 1000);
