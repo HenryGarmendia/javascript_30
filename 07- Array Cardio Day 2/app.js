e96fd3abeeb6f2323020a663f667d6937d591bfc
HenryGarmendia <me@henrygarmendia.com>
2017-12-21 22:12:24 -0600

    modified app.js, added a arrow function with implicit return

--- 07- Array Cardio Day 2/app.js
+++ 07- Array Cardio Day 2/app.js
@@ -42,9 +42,21 @@ console.log({is_adult});
 const all_adult = people.every(person => (new Date()).getFullYear() - person.year >= 19);
 console.log({all_adult});
 
-// Array.prototype.find()
+/* 
+	The find() method returns the value of the first element in the array that satisfies the provided testing function. Otherwise undefined is returned. 
+*/ // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+
+// const comment = comments.find(function(comment) {
+// 	if (comment.id === 823423) {
+// 		return true;
+// 	}
+// });
+
+// arrow function with implicit return
+const comment = comments.find(comment => (comment.id === 823423));
+console.log(comment);
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
